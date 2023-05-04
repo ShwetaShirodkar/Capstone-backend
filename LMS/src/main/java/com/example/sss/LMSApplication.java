3327974e194a87572e8434d298d74f1d6ecde2ec
ShwetaShirodkar <tasktask86@gmail.com>
2023-05-04 22:47:17 +0530

    safe

--- LMS/src/main/java/com/example/sss/LMSApplication.java
+++ LMS/src/main/java/com/example/sss/LMSApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SssApplication {
+public class LMSApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SssApplication.class, args);
+		SpringApplication.run(LMSApplication.class, args);
 	}
 
 }
